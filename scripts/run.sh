63f79338384582642ae482390f15c694f8f157c9
kavros <akavroulakis@gmail.com>
2018-11-09 16:02:38 +0000

    adds scripts that run loops on backend of cirrus

---
+++ scripts/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+AFFINITY_EXE=../build/bin/loops2
+AFFINITY_OUTPUT_FOLDER=../results/affinity
+AFFINITY_ERRORS_FOLDER=../errors/affinity
+
+for i in 1 2 4 6 8 12 16 
+do
+	qsub -o "$AFFINITY_OUTPUT_FOLDER/loops2_$i.txt" \
+     -e "$AFFINITY_ERRORS_FOLDER/loops_$i.txt" \
+     -v executable="$AFFINITY_EXE",numberOfThreads=$i loops.pbs		
+done
+
+
